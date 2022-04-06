685b5caad7d73f09e520e78ac021bd3ab7dff6a4
2022 카지노사이트 와 바카라사이트 모음 <102132982+sinage4080@users.noreply.github.com>
2022-04-06 17:57:50 +0900

    Add files via upload

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = '카지노컴퍼니'
+copyright = 'https://projectfluent.io/'
+author = '카지노컴퍼니'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    '카지노': ('https://projectfluent.io/', None),
+    '카지노사이트': ('https://projectfluent.io/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
